201d924977a74854a281a26468d03d5116876222
nogal.ogg <git.nogal@skiff.com>
2023-10-25 01:51:58 +0000

    asdf

---
+++ events/ready.js
@@ -0,0 +1,9 @@
+import Events from 'discord.js';
+
+export const Ready = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    console.log(`BOT is ready! Logged in as ${client.user.tag}`)
+  },
+};
